6ff414db51d125780aa1777294907f10e2f54051
Kuwata-Hiroki <155409190+Kuwata-Hiroki@users.noreply.github.com>
2024-04-09 20:11:59 +0900

    メインページの雛形を製作中

--- YS_WEB.js
+++ YS_WEB.js
@@ -3,7 +3,26 @@ window.onload = function () {
     $(".js-loader").delay(800).fadeOut(600); // 画像をフェードアウト
     $(".js-loader-bg").delay(1300).fadeOut(600); // 背景色をフェードアウト
   };
-
+  
+$(function () {
+    // ウィンドウをスクロールしたら…
+    $(window).scroll(function () {
+        // ウィンドウの高さを取得
+        const wHeight = $(window).height();
+        // スクロールした量を取得
+        const wScroll = $(window).scrollTop();
+        //それぞれのフェードインクラスに繰り返し指定
+        $(".fadeIn,.fadeInLeft,.fadeInRight").each(function () {
+                // それぞれのフェードインクラスのウィンドウからの高さを取得
+                const bPosition = $(this).offset().top;
+            // その数値にウィンドウの高さを引き、最後に200pxを足す
+            if (wScroll > bPosition - wHeight + 200) {
+                // フェードインさせるクラスを付与
+                $(this).addClass("fadeInAnime");
+            }
+        });
+    });
+});
 
 
   
